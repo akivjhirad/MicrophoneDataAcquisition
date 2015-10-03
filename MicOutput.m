020635430c35acdc885bfa28cc3f16179b318493
akivjhirad <akivjh@live.com>
2015-10-03 18:09:49 -0400

    Matlab serial comm. file

---
+++ MicOutput.m
@@ -0,0 +1,29 @@
+% NAME: Akiv Jhirad
+
+s = serial('COM8');     %choose COM port
+set(s, 'Baudrate', 9600);   %sets the Baudrate to the same thing as the Esduino
+s.Terminator = 'CR';      %define terminator sent by Esduino
+fopen(s);   %open the port
+
+i = 0;
+lHandle = line(nan, nan);
+
+while (1)    
+    i = i+1;        %increment x-axis
+    X = get(lHandle, 'XData');  %define x-axis as type XData
+    Y = get(lHandle, 'YData');  %define y-axis as type YData
+    
+    X = [X i];      % store X axis value
+    micOutput = fscanf(s);  % read serial data as char
+    micOutput = str2num(micOutput); %convert char to int for plot
+    micOutput = (micOutput / 4095) * 5; %normalize data to 0 - 5 volts
+    Y = [Y micOutput];  % store data in y-axis
+    
+    set(lHandle, 'XData', X, 'YData', Y);   %create plot
+    drawnow;    %show plot
+
+end
+
+fclose(s);  %close the port
+delete(s);  
+clear s;
